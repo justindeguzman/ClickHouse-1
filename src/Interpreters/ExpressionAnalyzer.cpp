spmjr/ClickHouse

Share PreparedSet-s between task of the same mutation to reduce mem usage

src/Interpreters/ExpressionAnalyzer.cpp
@@ -459,25 +459,37 @@ void ExpressionAnalyzer::tryMakeSetForIndexFromSubquery(const ASTPtr & subquery_
         return;
     }
 
-    auto interpreter_subquery = interpretSubquery(subquery_or_table_name, getContext(), {}, query_options);
-    auto io = interpreter_subquery->execute();
-    PullingAsyncPipelineExecutor executor(io.pipeline);
-
-    SetPtr set = std::make_shared<Set>(settings.size_limits_for_set, true, getContext()->getSettingsRef().transform_null_in);
-    set->setHeader(executor.getHeader().getColumnsWithTypeAndName());
-
-    Block block;
-    while (executor.pull(block))
+    auto build_set = [&] () -> ConstSetPtr
     {
-        if (block.rows() == 0)
-            continue;
+        auto interpreter_subquery = interpretSubquery(subquery_or_table_name, getContext(), {}, query_options);
+        auto io = interpreter_subquery->execute();
+        PullingAsyncPipelineExecutor executor(io.pipeline);
 
-        /// If the limits have been exceeded, give up and let the default subquery processing actions take place.
-        if (!set->insertFromBlock(block.getColumnsWithTypeAndName()))
-            return;
-    }
+        SetPtr set = std::make_shared<Set>(settings.size_limits_for_set, true, getContext()->getSettingsRef().transform_null_in);
+        set->setHeader(executor.getHeader().getColumnsWithTypeAndName());
 
-    set->finishInsert();
+        Block block;
+        while (executor.pull(block))
+        {
+            if (block.rows() == 0)
+                continue;
+
+            /// If the limits have been exceeded, give up and let the default subquery processing actions take place.
+            if (!set->insertFromBlock(block.getColumnsWithTypeAndName()))
+                return nullptr;
+        }
+
+        set->finishInsert();
+
+        return set;
+    };
+
+    auto set_cache = getContext()->getPreparedSetsCache();
+
+    auto set = set_cache ? set_cache->findOrBuild(set_key, build_set) : build_set();
+
+    if (!set)
+        return;
 
     prepared_sets->set(set_key, std::move(set));
 }

src/Interpreters/PreparedSets.cpp
@@ -106,4 +106,62 @@ QueryPlanPtr SubqueryForSet::detachSource()
     return res;
 }
 
+ConstSetPtr PreparedSetsCache::findOrBuild(const PreparedSetKey & key, const std::function<ConstSetPtr()> & build_set)
+{
+    auto* log = &Poco::Logger::get("PreparedSetsCache");
+
+    EntryPtr entry;
+    bool need_to_build_set = false;
+
+    /// Look for existing entry in the cache.
+    {
+        std::lock_guard lock(cache_mutex);
+
+        auto it = cache.find(key);
+        if (it != cache.end())
+        {
+            entry = it->second;
+        }
+        else
+        {
+            if (build_set == nullptr)
+                return nullptr;
+
+            /// Insert the entry into the cache so that other threads can find it and start waiting for the set.
+            entry = std::make_shared<Entry>();
+            entry->filled_set = entry->promise.get_future();
+            cache[key] = entry;
+            need_to_build_set = true;
+        }
+    }
+
+    if (need_to_build_set)
+    {
+        LOG_DEBUG(log, "Building set for key {}:{}", key.ast_hash.first, key.ast_hash.second);
+        try
+        {
+            auto set = build_set();
+            entry->promise.set_value(set);
+        }
+        catch (...)
+        {
+            entry->promise.set_exception(std::current_exception());
+            throw;
+        }
+
+        return entry->filled_set.get();
+    }
+
+    if (entry->filled_set.valid() && entry->filled_set.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+    {
+        LOG_DEBUG(log, "Found set for key {}:{} without wait", key.ast_hash.first, key.ast_hash.second);
+    }
+    else
+    {
+        LOG_DEBUG(log, "Found set for key {}:{} with wait", key.ast_hash.first, key.ast_hash.second);
+    }
+    return entry->filled_set.get();
+}
+
+
 };

src/Interpreters/PreparedSets.h
@@ -2,6 +2,7 @@
 
 #include <Parsers/IAST.h>
 #include <DataTypes/IDataType.h>
+#include <future>
 #include <memory>
 #include <unordered_map>
 #include <vector>
@@ -97,5 +98,32 @@ private:
 };
 
 using PreparedSetsPtr = std::shared_ptr<PreparedSets>;
+
+
+/// This set cache is used to avoid building the same set multiple times. It is different from PreparedSets in way that
+/// it can be used across multiple queries. One use case is when we execute the same mutation on multiple parts. In this
+/// case each part is processed by a separate mutation task but they can share the same set.
+class PreparedSetsCache
+{
+public:
+    /// Returns the set from the cache or builds it using the provided function.
+    /// If the set is already being built by another task, then this call will wait for the set to be built.
+    ConstSetPtr findOrBuild(const PreparedSetKey & key, const std::function<ConstSetPtr()> & build_set);
+
+private:
+    struct Entry
+    {
+        std::promise<ConstSetPtr> promise;          /// The promise is set when the set is built by the first task.
+        std::shared_future<ConstSetPtr> filled_set; /// Other tasks can wait for the set to be built.
+    };
+
+    using EntryPtr = std::shared_ptr<Entry>;
+
+    /// Protects just updates to the cache. When we got EntyPtr from the cache we can access it without locking.
+    std::mutex cache_mutex;
+    std::unordered_map<PreparedSetKey, EntryPtr, PreparedSetKey::Hash> cache;
+};
+
+using PreparedSetsCachePtr = std::shared_ptr<PreparedSetsCache>;
 
 }

justindeguzman/ClickHouse-1

src/Interpreters/ExpressionAnalyzer.cpp
@@ -459,25 +459,37 @@ void ExpressionAnalyzer::tryMakeSetForIndexFromSubquery(const ASTPtr & subquery_
         return;
     }
 
-    auto interpreter_subquery = interpretSubquery(subquery_or_table_name, getContext(), {}, query_options);
-    auto io = interpreter_subquery->execute();
-    PullingAsyncPipelineExecutor executor(io.pipeline);
-
-    SetPtr set = std::make_shared<Set>(settings.size_limits_for_set, true, getContext()->getSettingsRef().transform_null_in);
-    set->setHeader(executor.getHeader().getColumnsWithTypeAndName());
-
-    Block block;
-    while (executor.pull(block))
+    auto build_set = [&] () -> SetPtr
     {
-        if (block.rows() == 0)
-            continue;
+        auto interpreter_subquery = interpretSubquery(subquery_or_table_name, getContext(), {}, query_options);
+        auto io = interpreter_subquery->execute();
+        PullingAsyncPipelineExecutor executor(io.pipeline);
 
-        /// If the limits have been exceeded, give up and let the default subquery processing actions take place.
-        if (!set->insertFromBlock(block.getColumnsWithTypeAndName()))
-            return;
-    }
+        SetPtr set = std::make_shared<Set>(settings.size_limits_for_set, true, getContext()->getSettingsRef().transform_null_in);
+        set->setHeader(executor.getHeader().getColumnsWithTypeAndName());
 
-    set->finishInsert();
+        Block block;
+        while (executor.pull(block))
+        {
+            if (block.rows() == 0)
+                continue;
+
+            /// If the limits have been exceeded, give up and let the default subquery processing actions take place.
+            if (!set->insertFromBlock(block.getColumnsWithTypeAndName()))
+                return nullptr;
+        }
+
+        set->finishInsert();
+
+        return set;
+    };
+
+    auto set_cache = getContext()->getPreparedSetsCache();
+
+    auto set = set_cache ? set_cache->findOrBuild(set_key, build_set) : build_set();
+
+    if (!set)
+        return;
 
     prepared_sets->set(set_key, std::move(set));
 }

src/Interpreters/PreparedSets.cpp
@@ -106,4 +106,62 @@ QueryPlanPtr SubqueryForSet::detachSource()
     return res;
 }
 
+SetPtr PreparedSetsCache::findOrBuild(const PreparedSetKey & key, const std::function<SetPtr()> & build_set)
+{
+    auto* log = &Poco::Logger::get("PreparedSetsCache");
+
+    EntryPtr entry;
+    bool need_to_build_set = false;
+
+    /// Look for existing entry in the cache.
+    {
+        std::lock_guard lock(cache_mutex);
+
+        auto it = cache.find(key);
+        if (it != cache.end())
+        {
+            entry = it->second;
+        }
+        else
+        {
+            if (build_set == nullptr)
+                return nullptr;
+
+            /// Insert the entry into the cache so that other threads can find it and start waiting for the set.
+            entry = std::make_shared<Entry>();
+            entry->filled_set = entry->promise.get_future();
+            cache[key] = entry;
+            need_to_build_set = true;
+        }
+    }
+
+    if (need_to_build_set)
+    {
+        LOG_DEBUG(log, "Building set for key {}:{}", key.ast_hash.first, key.ast_hash.second);
+        try
+        {
+            auto set = build_set();
+            entry->promise.set_value(set);
+        }
+        catch (...)
+        {
+            entry->promise.set_exception(std::current_exception());
+            throw;
+        }
+
+        return entry->filled_set.get();
+    }
+
+    if (entry->filled_set.valid() && entry->filled_set.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+    {
+        LOG_DEBUG(log, "Found set for key {}:{} without wait", key.ast_hash.first, key.ast_hash.second);
+    }
+    else
+    {
+        LOG_DEBUG(log, "Found set for key {}:{} with wait", key.ast_hash.first, key.ast_hash.second);
+    }
+    return entry->filled_set.get();
+}
+
+
 };

src/Interpreters/PreparedSets.h
@@ -2,6 +2,7 @@
 
 #include <Parsers/IAST.h>
 #include <DataTypes/IDataType.h>
+#include <future>
 #include <memory>
 #include <unordered_map>
 #include <vector>
@@ -96,5 +97,32 @@ private:
 };
 
 using PreparedSetsPtr = std::shared_ptr<PreparedSets>;
+
+
+/// This set cache is used to avoid building the same set multiple times. It is different from PreparedSets in way that
+/// it can be used across multiple queries. One use case is when we execute the same mutation on multiple parts. In this
+/// case each part is processed by a separate mutation task but they can share the same set.
+class PreparedSetsCache
+{
+public:
+    /// Returns the set from the cache or builds it using the provided function.
+    /// If the set is already being built by another task, then this call will wait for the set to be built.
+    SetPtr findOrBuild(const PreparedSetKey & key, const std::function<SetPtr()> & build_set);
+
+private:
+    struct Entry
+    {
+        std::promise<SetPtr> promise;          /// The promise is set when the set is built by the first task.
+        std::shared_future<SetPtr> filled_set; /// Other tasks can wait for the set to be built.
+    };
+
+    using EntryPtr = std::shared_ptr<Entry>;
+
+    /// Protects just updates to the cache. When we got EntyPtr from the cache we can access it without locking.
+    std::mutex cache_mutex;
+    std::unordered_map<PreparedSetKey, EntryPtr, PreparedSetKey::Hash> cache;
+};
+
+using PreparedSetsCachePtr = std::shared_ptr<PreparedSetsCache>;
 
 }